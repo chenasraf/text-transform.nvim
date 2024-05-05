fb6bb28db79c826a7370e0c31157845ca707af90
Chen Asraf <casraf@pm.me>
2024-05-06 02:14:04 +0300

    refactor: remove main.lua

--- lua/text-transform/init.lua
+++ lua/text-transform/init.lua
@@ -1,5 +1,8 @@
-local M = require("text-transform.main")
 local utils = require("text-transform.utils")
+local tt = require("text-transform.transformers")
+local replacers = require("text-transform.replacers")
+local state = require("text-transform.state")
+local popup = require("text-transform.popup")
 
 local TextTransform = {}
 
@@ -7,6 +10,14 @@ function TextTransform.setup(opts)
   _G.TextTransform.config = require("text-transform.config").setup(opts)
 end
 
-_G.TextTransform = utils.merge(M, TextTransform)
+local function merge(table)
+  TextTransform = utils.merge(TextTransform, table)
+end
 
+merge(tt)
+merge(replacers)
+merge(state)
+merge(popup)
+
+_G.TextTransform = TextTransform
 return _G.TextTransform
